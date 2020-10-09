7086d1c0bc94aa8bb93f70772acf5e96a48c7f1a
ADI-KOTKAR <adityakotkar75@gmail.com>
2020-10-09 19:06:58 +0530

    text encode done

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ from modes.Video.video import video
 
 UPLOAD_IMAGE_FOLDER = 'modes\\Image\\static'
 IMAGE_CACHE_FOLDER = 'modes\\Image\\__pycache__'
+UPLOAD_TEXT_FOLDER = 'modes\\Text\\static'
+TEXT_CACHE_FOLDER = 'modes\\Text\\__pycache__'
 # ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.secret_key = "hello"
 app.config['UPLOAD_IMAGE_FOLDER'] = UPLOAD_IMAGE_FOLDER
 app.config['IMAGE_CACHE_FOLDER'] = IMAGE_CACHE_FOLDER
+app.config['UPLOAD_TEXT_FOLDER'] = UPLOAD_TEXT_FOLDER
+app.config['TEXT_CACHE_FOLDER'] = TEXT_CACHE_FOLDER
 app.register_blueprint(image, url_prefix="/image")
 app.register_blueprint(audio, url_prefix="/audio")
 app.register_blueprint(text, url_prefix="/text")
--- modes/Text/text.py
+++ modes/Text/text.py
@@ -1,11 +1,58 @@
-from flask import Blueprint, render_template
+import os
+from PIL import Image
+import stepic
+import shutil
+from flask import Blueprint, current_app, render_template, url_for, redirect, request, session, flash
+from datetime import timedelta
+from flask_wtf import FlaskForm
+from werkzeug.utils import secure_filename
 
 text = Blueprint("text", __name__, static_folder="static", template_folder="templates")
 
 @text.route("/encode")
 def text_encode():
+    if os.path.exists(current_app.config['TEXT_CACHE_FOLDER']):
+      shutil.rmtree(current_app.config['TEXT_CACHE_FOLDER'], ignore_errors = False)
+    else:
+      print("Not Found")
+    
+    if os.path.exists(os.path.join(current_app.config['UPLOAD_TEXT_FOLDER'], "encrypted_text_image.png")):
+      # print("Found")
+      os.remove(os.path.join(current_app.config['UPLOAD_TEXT_FOLDER'], "encrypted_text_image.png"))
+    else:
+      print("Not found")
     return render_template("encode-text.html")
+
+@text.route("/encode-result", methods = ['POST', 'GET'])
+def text_encode_result():
+    if request.method == 'POST':
+        message = request.form['message']
+        if 'file' not in request.files:
+            flash('No image found')
+        file = request.files['image']
+        
+        if file.filename == '':
+            flash('No image selected')
+        
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(current_app.config['UPLOAD_TEXT_FOLDER'], filename))
+            text_encryption = True
+            encrypt_text(os.path.join(current_app.config['UPLOAD_TEXT_FOLDER'], filename), message)
+        else:
+            text_encryption = False
+        result = request.form
+        
+        return render_template("encode-text-result.html",result = result, file=file, text_encryption=text_encryption, message=message)
+        
 
 @text.route("/decode")
 def text_decode():
     return render_template("decode-text.html")
+
+# Encryption function
+def encrypt_text(image_1, message):
+    im = Image.open(image_1)
+
+    im1 = stepic.encode(im, bytes(str(message), encoding='utf-8'))
+    im1.save(os.path.join(current_app.config['UPLOAD_TEXT_FOLDER'], "encrypted_text_image.png"))
